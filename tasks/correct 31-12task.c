49ad7eca6a2c71f5cc2f7bd270a0d5beafa044e1
muzammil2py <sheikhmuzu2@gmail.com>
2026-01-03 15:48:59 +0530

    Add files via upload

---
+++ tasks/correct 31-12task.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h> 
+#include <stdlib.h> // Needed for exit()
+
+int main()
+{
+    FILE *fp;
+    int choice;
+    // We need variables for User Input AND variables to hold data read from the file
+    char email[20], pass[20], balance[200];
+    char fileEmail[20], filePass[20], fileBalance[200]; 
+
+    printf("Press 1 for Sign up \nPress 2 for Login");
+    printf("\nPlease choose a number:- ");
+    scanf("%d", &choice);
+
+    switch(choice)
+    {
+    case 1: // SIGN UP
+        printf("Enter your email:- ");
+        scanf("%s", email);
+
+        printf("Enter the password:- ");
+        scanf("%s", pass);
+
+        printf("Enter the balance:- ");
+        scanf("%s", balance);
+
+        fp = fopen("details.txt", "w"); 
+
+        // 1. ADDING \n AS REQUESTED
+        // We use "%s\n" so every item is on a new line. 
+        // This is crucial for reading it back later.
+        fprintf(fp, "%s\n", email);
+        fprintf(fp, "%s\n", pass);
+        fprintf(fp, "%s\n", balance);
+        
+        fclose(fp); // 2. ALWAYS CLOSE THE FILE
+        printf("Account created successfully.\n");
+        break;
+
+    case 2: // LOGIN
+        printf("Enter the registered email:- ");
+        scanf("%s", email);
+
+        // 3. CORRECT FOPEN SYNTAX
+        fp = fopen("details.txt", "r"); // "r" for Read mode
+
+        if (fp == NULL) {
+            printf("No registered users found!\n");
+            return 0;
+        }
+
+        // 4. READ FILE INTO TEMPORARY VARIABLES
+        fscanf(fp, "%s", fileEmail);
+        fscanf(fp, "%s", filePass);
+        
+        // 5. COMPARE STRINGS USING STRCMP
+        // strcmp returns 0 if strings are identical
+        if(strcmp(email, fileEmail) != 0) 
+        {
+            printf("\nGiven email is not registered/correct.");
+            printf("\nPlease check your email once again!!");
+        }
+        else 
+        {
+            printf("Given email is correct. \n");
+            
+            printf("Enter the registered correct password:- ");
+            scanf("%s", pass);
+
+            if(strcmp(pass, filePass) != 0)
+            {
+                printf("Please check the password again");
+            }
+            else
+            {
+                printf("Your password was correct & you logged in successfully!");
+            }
+        }
+        fclose(fp);
+        break;
+    }
+    return 0;
+}
